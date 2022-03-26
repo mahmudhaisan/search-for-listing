a3da21f3e1687cecbf9b38dde8eac38f930af5bd
mahmudhaisan <mahmudhaisan@gmail.com>
2022-03-26 23:57:29 +0600

    add enque and fix style

--- search-for-listing.php
+++ search-for-listing.php
@@ -9,7 +9,7 @@
  * Author URI: http://mahmudhaisan.com/
  * Developer: Mahmud Haisan
  * Developer URI: http://mahmudhaisan.com/
- * Text Domain: Search
+ * Text Domain: Search493
  * Domain Path: /languages
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -22,21 +22,43 @@ if (!defined('ABSPATH')) {
 
 
 function search15()
-{ ?>
+{
 
-    <form action="/search" method="get">
-        <input type="text" name="search" id="" value="<?php echo $_GET['search']; ?>">
-        <input type="submit" name="" id="">
-    </form>
+
+?>
+
+
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+            <div class="content">
+                <div class="pull-middle">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <form action="/search" method="get">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search" name="search" id="" value="<?php echo $_GET['search']; ?>">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-success btn-circle" type="submit">Search</button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 
 
     <?php }
 
 
-// add short code 
+//add short code
 add_shortcode('search', 'search15');
-
-
 
 
 
@@ -53,8 +75,10 @@ function searchResult()
         );
         $query = get_posts($args);
         if ($query == array()) {
-            echo 'No results found';
+            echo 'No result found';
         }
+
+
         foreach ($query as $q) {
 
 
@@ -89,9 +113,9 @@ function searchResult()
                                     <div class="mt-5">
                                         <ul class="social-network social-circle">
                                             <li><a href="#" class="icoRss"><i class="fa fa-heart"></i></a></li>
-                                            <li><a href="#" class="icoFacebook"><i class="fa fa-thumbs-up"></i></a></li>
-                                            <li><a href="#" class="icoTwitter"><i class="fa fa-star"></i></a></li>
-                                            <li><a href="#" class="icoGoogle"><i class="fa fa-save"></i></a></li>
+                                            <li><a href="#" class="icoRss"><i class="fa fa-thumbs-up"></i></a></li>
+                                            <li><a href="#" class="icoRss"><i class="fa fa-star"></i></a></li>
+                                            <li><a href="#" class="icoRss"><i class="fa fa-share"></i></a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -104,17 +128,6 @@ function searchResult()
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -138,4 +151,39 @@ function searchResult()
 }
 
 
+
+
 add_shortcode('results', 'searchResult');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//enqueue files
+
+
+function search493_enqueue()
+{
+    $enq = plugin_dir_url(__FILE__) . 'assets/';
+
+    // style
+    wp_enqueue_style('bootstrap', $enq . 'css/bootstrap.min.css');
+    wp_enqueue_style('fontawesome', $enq . 'css/font-awesome.min.css');
+    wp_enqueue_style('style', $enq . 'css/style.css');
+
+    //scripts
+    wp_enqueue_script('bootstrap', $enq . 'js/bootstrap.min.js');
+    wp_enqueue_script('script', $enq . 'js/script.js');
+}
+
+
+add_action('wp_enqueue_scripts', 'search493_enqueue');
